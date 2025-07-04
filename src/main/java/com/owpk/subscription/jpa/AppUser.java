2c550c7f8212a79879b72cf260355eceb4fab6f1
owpk <vzvz4@mail.ru>
2025-07-05 03:24:33 +0900

    MAIN: fix generating id app user

--- src/main/java/com/owpk/subscription/jpa/AppUser.java
+++ src/main/java/com/owpk/subscription/jpa/AppUser.java
@@ -2,15 +2,12 @@ package com.owpk.subscription.jpa;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class AppUser {
     @Id
     @Column(name = "username", unique = true)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userName;
     private String password;
     private String role;
